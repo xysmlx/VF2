b1402516493e2474c88528b74597ad6f1bfdadb3
xysmlx <xysmlx@163.com>
2015-12-03 23:21:22 +0800

    Need debug

--- Code/VF2/VF2/VF2.cpp
+++ Code/VF2/VF2/VF2.cpp
@@ -2,282 +2,430 @@
 
 void VF2::init(const vector<Graph> &db)
 {
-    DBGraph.clear();
-    DBGraph = db;
-    match.clear();
+	DBGraph.clear();
+	DBGraph = db;
+	match.clear();
 }
 
 void VF2::GenRevGraph(const Graph &src, Graph &dst)
 {
-    dst = Graph();
-    for (int i = 0; i < src.vn; i++)
-        dst.addv(src.vtx[i].id, src.vtx[i].label);
+	dst = Graph();
+	for (int i = 0; i < src.vn; i++)
+		dst.addv(src.vtx[i].id, src.vtx[i].label);
 
-    for (int i = 0; i < src.en; i++)
-        dst.addse(src.edge[i].v, src.edge[i].u, src.edge[i].label);
+	for (int i = 0; i < src.en; i++)
+		dst.addse(src.edge[i].v, src.edge[i].u, src.edge[i].label);
 }
 
-bool VF2::CheckPrev(const State &s, int a, int b)
+bool VF2::CheckPrev(const State &s)
 {
-    return 0;
+	vector<int> tmp;
+	bool flag;
+	vector<int>::iterator ite;
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(m1.begin(), m1.end(), pred1.begin(), pred1.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	for (auto ite1 = tmp.begin();ite1 != tmp.end();ite1++)
+	{
+		flag = 0;
+		for (auto ite2 = pred2.begin();ite2 != pred2.end() && !flag;ite2++)
+			if (s.core1[*ite1] == *ite2) flag = 1;
+		if (!flag) return 0;
+	}
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(m2.begin(), m2.end(), pred2.begin(), pred2.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	for (auto ite2 = tmp.begin();ite2 != tmp.end();ite2++)
+	{
+		flag = 0;
+		for (auto ite1 = pred1.begin();ite1 != pred1.end() && !flag;ite1++)
+			if (s.core2[*ite2] == *ite1) flag = 1;
+		if (!flag) return 0;
+	}
+
+	return 1;
 }
 
-bool VF2::CheckSucc(const State &s, int a, int b)
+bool VF2::CheckSucc(const State &s)
 {
-    return 0;
+	vector<int> tmp;
+	bool flag;
+	vector<int>::iterator ite;
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(m1.begin(), m1.end(), succ1.begin(), succ1.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	for (auto ite1 = tmp.begin();ite1 != tmp.end();ite1++)
+	{
+		flag = 0;
+		for (auto ite2 = succ2.begin();ite2 != succ2.end() && !flag;ite2++)
+			if (s.core1[*ite1] == *ite2) flag = 1;
+		if (!flag) return 0;
+	}
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(m2.begin(), m2.end(), succ2.begin(), succ2.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	for (auto ite2 = tmp.begin();ite2 != tmp.end();ite2++)
+	{
+		flag = 0;
+		for (auto ite1 = succ1.begin();ite1 != succ1.end() && !flag;ite1++)
+			if (s.core2[*ite2] == *ite1) flag = 1;
+		if (!flag) return 0;
+	}
+
+	return 1;
 }
 
-bool VF2::CheckIn(const State &s, int a, int b)
+bool VF2::CheckIn(const State &s)
 {
-    return 0;
+	vector<int> tmp;
+	int a, b, c, d;
+	vector<int>::iterator ite;
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(succ1.begin(), succ1.end(), tin1.begin(), tin1.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	a = tmp.size();
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(succ2.begin(), succ2.end(), tin2.begin(), tin2.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	b = tmp.size();
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(pred1.begin(), pred1.end(), tin1.begin(), tin1.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	c = tmp.size();
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(pred2.begin(), pred2.end(), tin2.begin(), tin2.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	d = tmp.size();
+
+	return (a >= b) && (c >= d);
 }
 
-bool VF2::CheckOut(const State &s, int a, int b)
+bool VF2::CheckOut(const State &s)
 {
-    return 0;
+	vector<int> tmp;
+	int a, b, c, d;
+	vector<int>::iterator ite;
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(succ1.begin(), succ1.end(), tout1.begin(), tout1.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	a = tmp.size();
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(succ2.begin(), succ2.end(), tout2.begin(), tout2.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	b = tmp.size();
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(pred1.begin(), pred1.end(), tout1.begin(), tout1.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	c = tmp.size();
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(pred2.begin(), pred2.end(), tout2.begin(), tout2.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	d = tmp.size();
+
+	return (a >= b) && (c >= d);
 }
 
-bool VF2::CheckNew(const State &s, int a, int b)
+bool VF2::CheckNew(const State &s)
 {
-    return 0;
+	vector<int> tmp;
+	int a, b, c, d;
+	vector<int>::iterator ite;
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(ns1.begin(), ns1.end(), pred1.begin(), pred1.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	a = tmp.size();
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(ns2.begin(), ns2.end(), pred2.begin(), pred2.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	b = tmp.size();
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(ns1.begin(), ns1.end(), succ1.begin(), succ1.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	c = tmp.size();
+
+	tmp.resize(Graph::maxv);
+	ite = set_intersection(ns2.begin(), ns2.end(), succ2.begin(), succ2.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	d = tmp.size();
+
+	return (a >= b) && (c >= d);
 }
 
 void VF2::CalDFSVec(const State &s)
 {
-    m1.clear();
-    m2.clear();
-    tin1.clear();
-    tin2.clear();
-    tout1.clear();
-    tout2.clear();
-    n1.clear();
-    n2.clear();
-    ns1.clear();
-    ns2.clear();
-    t1.clear();
-    t2.clear();
+	m1.clear();
+	m2.clear();
+	tin1.clear();
+	tin2.clear();
+	tout1.clear();
+	tout2.clear();
+	n1.clear();
+	n2.clear();
+	ns1.clear();
+	ns2.clear();
+	t1.clear();
+	t2.clear();
 
-    for (auto ite = s.s.begin(); ite != s.s.end(); ite++)
-    {
-        m1.push_back(ite->first);
-        m2.push_back(ite->second);
-    }
-    sort(m1.begin(), m1.end());
-    sort(m2.begin(), m2.end());
+	for (auto ite = s.s.begin(); ite != s.s.end(); ite++)
+	{
+		m1.push_back(ite->first);
+		m2.push_back(ite->second);
+	}
+	sort(m1.begin(), m1.end());
+	sort(m2.begin(), m2.end());
 
-    for (int i = 0; i < pat.vn; i++)
-        if (s.out1[i])
-            tout1.push_back(i);
-    for (int i = 0; i < g.vn; i++)
-        if (s.out2[i])
-            tout2.push_back(i);
+	for (int i = 0; i < pat.vn; i++)
+		if (s.out1[i])
+			tout1.push_back(i);
+	for (int i = 0; i < g.vn; i++)
+		if (s.out2[i])
+			tout2.push_back(i);
 
-    for (int i = 0; i < pat.vn; i++)
-        if (s.in1[i])
-            tin1.push_back(i);
-    for (int i = 0; i < g.vn; i++)
-        if (s.in2[i])
-            tin2.push_back(i);
+	for (int i = 0; i < pat.vn; i++)
+		if (s.in1[i])
+			tin1.push_back(i);
+	for (int i = 0; i < g.vn; i++)
+		if (s.in2[i])
+			tin2.push_back(i);
 
-    for (int i = 0; i < pat.vn; i++)
-        ns1.push_back(i);
-    for (int i = 0; i < g.vn; i++)
-        ns2.push_back(i);
+	for (int i = 0; i < pat.vn; i++)
+		n1.push_back(i);
+	for (int i = 0; i < g.vn; i++)
+		n2.push_back(i);
 
-    set_union(tin1.begin(), tin1.end(), tout1.begin(), tout1.end(), t1.begin());
-    set_union(tin1.begin(), tin1.end(), tout2.begin(), tout2.end(), t2.begin());
+	t1.resize(Graph::maxv);
+	auto ite = set_union(tin1.begin(), tin1.end(), tout1.begin(), tout1.end(), t1.begin());
+	t1.erase(ite, t1.end());
 
-    vector<int> tmp(Graph::maxv);
+	t2.resize(Graph::maxv);
+	ite = set_union(tin1.begin(), tin1.end(), tout2.begin(), tout2.end(), t2.begin());
+	t2.erase(ite, t2.end());
 
-    sort(n1.begin(), n1.end());
-    sort(n2.begin(), n2.end());
+	vector<int> tmp;
+
+	tmp.resize(Graph::maxv);
+	ite = set_difference(n1.begin(), n1.end(), m1.begin(), m1.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	ns1.resize(Graph::maxv);
+	ite = set_difference(tmp.begin(), tmp.end(), t1.begin(), t1.end(), ns1.begin());
+	ns1.erase(ite, ns1.end());
+
+	tmp.resize(Graph::maxv);
+	ite = set_difference(n2.begin(), n2.end(), m2.begin(), m2.end(), tmp.begin());
+	tmp.erase(ite, tmp.end());
+	ns2.resize(Graph::maxv);
+	ite = set_difference(tmp.begin(), tmp.end(), t2.begin(), t2.end(), ns2.begin());
+	ns2.erase(ite, ns2.end());
 }
 
 void VF2::CalCheckVec(const State &s, int a, int b)
 {
-    // Init
-    pred1.clear();
-    pred2.clear();
-    succ1.clear();
-    succ2.clear();
+	// Init
+	pred1.clear();
+	pred2.clear();
+	succ1.clear();
+	succ2.clear();
 
-    // aPred
-    for (int i = revpat.head[a]; ~i; i = revpat.edge[i].next)
-        pred1.push_back(revpat.edge[i].v);
-    // bPred
-    for (int i = revg.head[b]; ~i; i = revg.edge[i].next)
-        pred2.push_back(revg.edge[i].v);
-    // aSucc
-    for (int i = pat.head[a]; ~i; i = pat.edge[i].next)
-        succ1.push_back(pat.edge[i].v);
-    // bSucc
-    for (int i = g.head[b]; ~i; i = g.edge[i].next)
-        succ2.push_back(g.edge[i].v);
+	// aPred
+	for (int i = revpat.head[a]; ~i; i = revpat.edge[i].next)
+		pred1.push_back(revpat.edge[i].v);
+	// bPred
+	for (int i = revg.head[b]; ~i; i = revg.edge[i].next)
+		pred2.push_back(revg.edge[i].v);
+	// aSucc
+	for (int i = pat.head[a]; ~i; i = pat.edge[i].next)
+		succ1.push_back(pat.edge[i].v);
+	// bSucc
+	for (int i = g.head[b]; ~i; i = g.edge[i].next)
+		succ2.push_back(g.edge[i].v);
 
-    vector<int> tmp;
-    tmp = ;
+	vector<int> tmp;
+	tmp.reserve(Graph::maxv);
 
-    // Sort
-    sort(pred1.begin(), pred1.end());
-    sort(pred2.begin(), pred2.end());
-    sort(succ1.begin(), succ1, end());
-    sort(succ2.begin(), succ2.end());
+	// Sort
+	sort(pred1.begin(), pred1.end());
+	sort(pred2.begin(), pred2.end());
+	sort(succ1.begin(), succ1.end());
+	sort(succ2.begin(), succ2.end());
 }
 
 bool VF2::check(const State &s, int a, int b)
 {
-    CalVec(s, a, b);
+	if (pat.vtx[a].label != g.vtx[b].label) return 0; // Check label
 
-    if (CheckPrev(s, a, b) && CheckSucc(s, a, b) && CheckIn(s, a, b) && CheckOut(s, a, b) && CheckNew(s, a, b))
-        return 1;
-    return 0;
+	CalCheckVec(s, a, b);
+
+	if (CheckPrev(s) && CheckSucc(s) && CheckIn(s) && CheckOut(s) && CheckNew(s)) return 1;
+	return 0;
 }
 
 void VF2::GenPairs(const State &s)
 {
-    CalDFSVec(s);
+	CalDFSVec(s);
 
-    flagIn = flagOut = flagAll = 0;
-    allPairs.clear();
+	flagIn = flagOut = flagAll = 0;
+	allPairs.clear();
 
-    for (int i = 0; i < (int)tout1.size(); i++)
-        for (int j = 0; j < (int)tout2.size(); j++)
-            allPairs.push_back(make_pair(tout1[i], tout2[j]));
-    if (!allPairs.empty())
-    {
-        flagOut = 1;
-        return;
-    }
+	for (int i = 0; i < (int)tout1.size(); i++)
+		for (int j = 0; j < (int)tout2.size(); j++)
+			allPairs.push_back(make_pair(tout1[i], tout2[j]));
+	if (!allPairs.empty())
+	{
+		flagOut = 1;
+		return;
+	}
 
-    for (int i = 0; i < (int)tin1.size(); i++)
-        for (int j = 0; j < (int)tin2.size(); j++)
-            allPairs.push_back(make_pair(tin1[i], tin2[j]));
-    if (!allPairs.empty())
-    {
-        flagIn = 1;
-        return;
-    }
+	for (int i = 0; i < (int)tin1.size(); i++)
+		for (int j = 0; j < (int)tin2.size(); j++)
+			allPairs.push_back(make_pair(tin1[i], tin2[j]));
+	if (!allPairs.empty())
+	{
+		flagIn = 1;
+		return;
+	}
 
-    vector<int> t1, t2;
-    t1.clear();
-    t2.clear();
-    for (int i = 0; i < pat.vn; i++)
-        if (s.core1[i] == -1)
-            t1.push_back(i);
-    for (int i = 0; i < g.vn; i++)
-        if (s.core2[i] == -1)
-            t2.push_back(i);
-    for (int i = 0; i < (int)t1.size(); i++)
-        for (int j = 0; j < (int)t2.size(); j++)
-            allPairs.push_back(make_pair(t1[i], t2[j]));
-    flagAll = 1;
+	vector<int> t1, t2;
+	t1.clear();
+	t2.clear();
+	for (int i = 0; i < pat.vn; i++)
+		if (s.core1[i] == -1)
+			t1.push_back(i);
+	for (int i = 0; i < g.vn; i++)
+		if (s.core2[i] == -1)
+			t2.push_back(i);
+	for (int i = 0; i < (int)t1.size(); i++)
+		for (int j = 0; j < (int)t2.size(); j++)
+			allPairs.push_back(make_pair(t1[i], t2[j]));
+	flagAll = 1;
 }
 
 void VF2::CheckPairs(const State &s)
 {
-    candiPairs.clear();
+	candiPairs.clear();
 
-    for (auto ite = allPairs.begin(); ite != allPairs.end(); ite++)
-        if (check(s, ite->first, ite->second))
-            candiPairs.push_back(*ite);
+	for (auto ite = allPairs.begin(); ite != allPairs.end(); ite++)
+		if (check(s, ite->first, ite->second))
+			candiPairs.push_back(*ite);
 }
 
 void VF2::UpdateState(State &s, int a, int b)
 {
-    // Update core,in,out
-    for (int i = 0; i < pat.vn; i++)
-    {
-        s.core1[a] = b;
-        s.in1[a] = 0;
-        s.out1[a] = 0;
-    }
-    for (int i = 0; i < g.vn; i++)
-    {
-        s.core2[b] = a;
-        s.in2[b] = 0;
-        s.out2[b] = 0;
-    }
+	// Update core,in,out
+	for (int i = 0; i < pat.vn; i++)
+	{
+		s.core1[a] = b;
+		s.in1[a] = 0;
+		s.out1[a] = 0;
+	}
+	for (int i = 0; i < g.vn; i++)
+	{
+		s.core2[b] = a;
+		s.in2[b] = 0;
+		s.out2[b] = 0;
+	}
 
-    // Add new out1
-    for (int i = pat.head[a]; ~i; i = pat.edge[i].next)
-    {
-        int v = pat.edge[i].v;
-        if (s.core1[v] == -1)
-            s.out1[v] = 1;
-    }
-    // Add new in1
-    for (int i = revpat.head[a]; ~i; i = revpat.edge[i].next)
-    {
-        int v = revpat.edge[i].v;
-        if (s.core1[v] == -1)
-            s.in1[v] = 1;
-    }
-    // Add new out2
-    for (int i = g.head[b]; ~i; i = g.edge[i].next)
-    {
-        int v = g.edge[i].v;
-        if (s.core2[v] == -1)
-            s.out2[v] = 1;
-    }
-    // Add new in2
-    for (int i = revg.head[b]; ~i; i = revg.edge[i].next)
-    {
-        int v = revg.edge[i].v;
-        if (s.core2[v] == -1)
-            s.in2[v] = 1;
-    }
+	// Add new out1
+	for (int i = pat.head[a]; ~i; i = pat.edge[i].next)
+	{
+		int v = pat.edge[i].v;
+		if (s.core1[v] == -1)
+			s.out1[v] = 1;
+	}
+	// Add new in1
+	for (int i = revpat.head[a]; ~i; i = revpat.edge[i].next)
+	{
+		int v = revpat.edge[i].v;
+		if (s.core1[v] == -1)
+			s.in1[v] = 1;
+	}
+	// Add new out2
+	for (int i = g.head[b]; ~i; i = g.edge[i].next)
+	{
+		int v = g.edge[i].v;
+		if (s.core2[v] == -1)
+			s.out2[v] = 1;
+	}
+	// Add new in2
+	for (int i = revg.head[b]; ~i; i = revg.edge[i].next)
+	{
+		int v = revg.edge[i].v;
+		if (s.core2[v] == -1)
+			s.in2[v] = 1;
+	}
+
+	// Add to s
+	s.s.push_back(make_pair(a, b));
 }
 
 bool VF2::dfs(const State &s)
 {
-    // Matched
-    if ((int)s.s.size() == pat.vn)
-    {
-        tlist = s.s;
-        return 1;
-    }
+	// Matched
+	if ((int)s.s.size() == pat.vn)
+	{
+		tlist = s.s;
+		return 1;
+	}
 
-    // Generate Pair(n,m)
-    GenPairs(s);
-    // Check allPairs, get candiPairs
-    CheckPairs(s);
+	// Generate Pair(n,m)
+	GenPairs(s);
+	// Check allPairs, get candiPairs
+	CheckPairs(s);
 
-    // Next recursive
-    for (auto ite = candiPairs.begin(); ite != candiPairs.end(); ite++)
-    {
-        State ns = s;
-        int a = ite->first;
-        int b = ite->second;
-        UpdateState(ns, a, b);
-        dfs(ns);
-    }
+	vector<prii> vec = candiPairs;
 
-    return 0;
+	//cout << vec.size() << endl;
+
+	// Next recursive
+	for (auto ite = vec.begin(); ite != vec.end(); ite++)
+	{
+		State ns = s;
+		int a = ite->first;
+		int b = ite->second;
+		UpdateState(ns, a, b);
+		dfs(ns);
+	}
+
+	return 0;
 }
 
 bool VF2::query()
 {
-    return dfs(State());
+	return dfs(State());
 }
 
 int VF2::vf2(const Graph &QG, const int &QID)
 {
-    QueryGraph = QG;
-    QueryID = QID;
+	QueryGraph = QG;
+	QueryID = QID;
 
-    pat = QueryGraph;
-    GenRevGraph(pat, revpat);
+	pat = QueryGraph;
+	GenRevGraph(pat, revpat);
 
-    int ret = 0;
-    for (int i = 0; i < (int)DBGraph.size(); i++)
-    {
-        g = DBGraph[i];
-        GenRevGraph(g, revg);
-        if (query()) // Matched
-        {
-            match.push_back(Match(tlist, i));
-            ret++;
-        }
-    }
-    return ret;
+	int ret = 0;
+	for (int i = 0; i < (int)DBGraph.size(); i++)
+	{
+		g = DBGraph[i];
+		GenRevGraph(g, revg);
+		if (query()) // Matched
+		{
+			match.push_back(Match(tlist, i));
+			ret++;
+		}
+	}
+	return ret;
 }
--- Code/VF2/VF2/VF2.h
+++ Code/VF2/VF2/VF2.h
@@ -6,80 +6,80 @@
 
 class VF2
 {
-  public:
-    struct State // State of dfs matching
-    {
-        vector<prii> s; // Store matched pairs
-        // Same means with the paper
-        int core1[Graph::maxv]; // core1[n] = m or -1
-        int core2[Graph::maxv]; // core2[m] = n or -1
-        bool in1[Graph::maxv];
-        bool in2[Graph::maxv];
-        bool out1[Graph::maxv];
-        bool out2[Graph::maxv];
-        State()
-        {
-            s.clear();
-            memset(core1, -1, sizeof(core1));
-            memset(core2, -1, sizeof(core2));
-            memset(in1, 0, sizeof(in1));
-            memset(in2, 0, sizeof(in2));
-            memset(out1, 0, sizeof(out1));
-            memset(out2, 0, sizeof(out2));
-        }
-    };
+public:
+	struct State // State of dfs matching
+	{
+		vector<prii> s; // Store matched pairs
+		// Same means with the paper
+		int core1[Graph::maxv]; // core1[n] = m or -1
+		int core2[Graph::maxv]; // core2[m] = n or -1
+		bool in1[Graph::maxv];
+		bool in2[Graph::maxv];
+		bool out1[Graph::maxv];
+		bool out2[Graph::maxv];
+		State()
+		{
+			s.clear();
+			memset(core1, -1, sizeof(core1));
+			memset(core2, -1, sizeof(core2));
+			memset(in1, 0, sizeof(in1));
+			memset(in2, 0, sizeof(in2));
+			memset(out1, 0, sizeof(out1));
+			memset(out2, 0, sizeof(out2));
+		}
+	};
 
-    struct Match // Store match
-    {
-        vector<prii> s; // match
-        int id;         // graphDB id
-        Match() {}
-        Match(vector<prii> _s, int _id) : s(_s), id(_id) {}
-    };
+	struct Match // Store match
+	{
+		vector<prii> s; // match
+		int id;         // graphDB id
+		Match() {}
+		Match(vector<prii> _s, int _id) : s(_s), id(_id) {}
+	};
 
-  public:
-    VF2() {}
-    void init(const vector<Graph> &db);             // Init the VF2 class
-    void GenRevGraph(const Graph &src, Graph &dst); // Generate reversed graph
-    bool CheckPrev(const State &s, int a, int b);   // Prev
-    bool CheckSucc(const State &s, int a, int b);   // Succ
-    bool CheckIn(const State &s, int a, int b);     // In
-    bool CheckOut(const State &s, int a, int b);    // Out
-    bool CheckNew(const State &s, int a, int b);    // New
-    void CalDFSVec(const State &s);                 // Cal all temp vec per dfs
-    void CalCheckVec(const State &s, int a, int b); // Cal all temp vec per check
-    bool check(const State &s, int a, int b);       // Check feasibility
-    void GenPairs(const State &s);                  // Generate all pairs to var allPairs
-    void CheckPairs(const State &s);                // Check allPairs, return candiPairs
-    void UpdateState(State &s, int a, int b);       // Update state ns with pair(a,b)
-    bool dfs(const State &s);                       // VF2 dfs (recursive)
-    bool query();                                   // Run VF2 on pat & g (main procedure)
-    int vf2(const Graph &QG, const int &QID);       // Run VF2 on QueryGraph & DBGraph (engine)
+public:
+	VF2() {}
+	void init(const vector<Graph> &db);             // Init the VF2 class
+	void GenRevGraph(const Graph &src, Graph &dst); // Generate reversed graph
+	bool CheckPrev(const State &s);					// Prev
+	bool CheckSucc(const State &s);   				// Succ
+	bool CheckIn(const State &s);     				// In
+	bool CheckOut(const State &s);    				// Out
+	bool CheckNew(const State &s);    				// New
+	void CalDFSVec(const State &s);                 // Cal all temp vec per dfs
+	void CalCheckVec(const State &s, int a, int b); // Cal all temp vec per check
+	bool check(const State &s, int a, int b);       // Check feasibility
+	void GenPairs(const State &s);                  // Generate all pairs to var allPairs
+	void CheckPairs(const State &s);                // Check allPairs, return candiPairs
+	void UpdateState(State &s, int a, int b);       // Update state ns with pair(a,b)
+	bool dfs(const State &s);                       // VF2 dfs (recursive)
+	bool query();                                   // Run VF2 on pat & g (main procedure)
+	int vf2(const Graph &QG, const int &QID);       // Run VF2 on QueryGraph & DBGraph (engine)
 
-  public:
-    vector<Graph> DBGraph; // Graph database
-    Graph QueryGraph;      // Graph for query, pattern
-    int QueryID;           // Store the Query ID
+public:
+	vector<Graph> DBGraph; // Graph database
+	Graph QueryGraph;      // Graph for query, pattern
+	int QueryID;           // Store the Query ID
 
-    vector<Match> match; // Store the matched graph info
+	vector<Match> match; // Store the matched graph info
 
-  private:
-    // Temp var
-    vector<prii> tlist;            // Store temp list of match
-    vector<prii> allPairs;         // Generated pairs
-    vector<prii> candiPairs;       // Candidate pairs pass check()
-    bool flagIn, flagOut, flagAll; // Flags
-    vector<int> pred1, pred2;      // Pred of a in pat & b in g
-    vector<int> succ1, succ2;      // Succ of a in pat & b in g
-    vector<int> m1, m2;            // M_1, M_2
-    vector<int> tin1, tin2;        // Tin_1,Tin_2
-    vector<int> tout1, tout2;      // Tout_1,Tout_2
-    vector<int> n1, n2;            // N_1, N_2
-    vector<int> ns1, ns2;          // Point set of pat & g
-    vector<int> t1, t2;            // tin+tout
+private:
+	// Temp var
+	vector<prii> tlist;            // Store temp list of match
+	vector<prii> allPairs;         // Generated pairs
+	vector<prii> candiPairs;       // Candidate pairs pass check()
+	bool flagIn, flagOut, flagAll; // Flags
+	vector<int> pred1, pred2;      // Pred of a in pat & b in g
+	vector<int> succ1, succ2;      // Succ of a in pat & b in g
+	vector<int> m1, m2;            // M_1, M_2
+	vector<int> tin1, tin2;        // Tin_1,Tin_2
+	vector<int> tout1, tout2;      // Tout_1,Tout_2
+	vector<int> n1, n2;            // N_1, N_2
+	vector<int> ns1, ns2;          // Point set of pat & g
+	vector<int> t1, t2;            // tin+tout
 
-    Graph pat, g;       // Pattern & DBGraph
-    Graph revpat, revg; // Reversed Pattern & DBGraph
+	Graph pat, g;       // Pattern & DBGraph
+	Graph revpat, revg; // Reversed Pattern & DBGraph
 };
 
 #endif
